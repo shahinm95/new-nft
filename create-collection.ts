d622133d3716b686cfe623ed0fe3877651b00e1e
John Doe <shahin.movazzaf@gmail.com>
2025-10-13 22:44:21 +0330

    no idea what am doing :))

--- create-collection.ts
+++ create-collection.ts
@@ -2,8 +2,22 @@ import { createNft,fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundat
 import { airdropIfRequired , getExplorerLink, getKeypairFromFile  } from "@solana-developers/helpers";
 import { createUmi  } from "@metaplex-foundation/umi-bundle-defaults";
 import {Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
-
+import {keypairIdentity} from "@metaplex-foundation/umi"
 
 
 const connection = new Connection(clusterApiUrl("devnet"));
-const user = getKeypairFromFile() //if we don't specify anything here it will use id.json which is in our home folder
+const user = await getKeypairFromFile() //if we don't specify anything here it will use id.json which is in our home folder
+
+await airdropIfRequired(connection, user.publicKey, 1 *LAMPORTS_PER_SOL, 0.5 *LAMPORTS_PER_SOL);
+ 
+console.log("loaded user" , user.publicKey.toBase58() );
+
+//we talk to metaplaces tools via Umi , soo lets define Umi
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata())
+
+//this is the copy of user but in format that umi uses 
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("set up Umi isntance fir user")
