2df0da9c47f328460321b3182db3d2bba3cbb83c
John Doe <shahin.movazzaf@gmail.com>
2025-10-13 22:28:45 +0330

    added neccesary imports and defined user and connection

---
+++ create-collection.ts
@@ -0,0 +1,9 @@
+import { createNft,fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { airdropIfRequired , getExplorerLink, getKeypairFromFile  } from "@solana-developers/helpers";
+import { createUmi  } from "@metaplex-foundation/umi-bundle-defaults";
+import {Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+
+
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const user = getKeypairFromFile() //if we don't specify anything here it will use id.json which is in our home folder
